2c2ca57914c6b4db696fce16ff6b4e1b4f605cd1
Brad McCoy <bradmccoydev@gmail.com>
2022-07-07 21:23:27 +1000

    update notifier
    Signed-off-by: Brad McCoy <bradmccoydev@gmail.com>

--- pkg/notifier/notifier.go
+++ pkg/notifier/notifier.go
@@ -1,55 +1,35 @@
 package notifier
 
 import (
+	"bytes"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
-	"time"
 
-	"github.com/hashicorp/go-retryablehttp"
 	"github.com/keptn-sandbox/keptn-flux-integration/pkg/provider"
 )
 
 func PostMessage(event provider.KeptnEvent) error {
-	httpClient := retryablehttp.NewClient()
-
-	httpClient.HTTPClient.Timeout = 15 * time.Second
-	httpClient.RetryWaitMin = 2 * time.Second
-	httpClient.RetryWaitMax = 30 * time.Second
-	httpClient.RetryMax = 4
-	httpClient.Logger = nil
-
-	// data, err := json.Marshal(event.Body)
-	// fmt.Println("*****")
-	// fmt.Println(event.Body)
-	// fmt.Println("*****")
-	// if err != nil {
-	// 	return fmt.Errorf("marshalling notification payload failed: %w", err)
-	// }
-
-	req, err := retryablehttp.NewRequest(http.MethodPost, event.Address, event.Body)
-	if err != nil {
-		return fmt.Errorf("failed to create a new request: %w", err)
-	}
-
+	var jsonStr = []byte(event.Body)
+	req, err := http.NewRequest("POST", event.Address, bytes.NewBuffer(jsonStr))
+	fmt.Println(string(jsonStr))
 	for k, v := range event.Headers {
 		headerKey := fmt.Sprintf("%s", k)
 		headerValue := fmt.Sprintf("%s", v)
 		req.Header.Set(headerKey, headerValue)
 	}
 
-	resp, err := httpClient.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to execute request: %w", err)
+		panic(err)
 	}
+	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusAccepted && resp.StatusCode != http.StatusCreated {
-		b, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("unable to read response body, %s", err)
-		}
-		return fmt.Errorf("request failed with status code %d, %s", resp.StatusCode, string(b))
-	}
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 
 	return nil
 }
